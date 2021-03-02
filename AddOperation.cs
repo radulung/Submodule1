d7123e51f58110f802d72fc4aad665d138c7eecb
radu.lung <radu.lung@tss-yonder.com>
2021-03-02 22:39:55 +0200

    Implement Add operation

---
+++ AddOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace Submodule1
+{
+    public class AddOperation
+    {
+        public int Execute(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
